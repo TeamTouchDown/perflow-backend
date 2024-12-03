a732f96abb9837f5db7419e92671d666054f7d93
kimjm9911 <kimjm9911@naver.com>
2024-12-03 09:49:42 +0900

    feat: #19 status 추가

---
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/payroll/command/domain/aggregate/PayrollStatus.java
@@ -0,0 +1,10 @@
+package com.touchdown.perflowbackend.payroll.command.domain.aggregate;
+
+public enum PayrollStatus {
+
+    PENDING,
+    CONFIRMED,
+    REJECTED,
+    COMPLETE
+
+}
---
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/payroll/command/domain/aggregate/SeverancePayStatus.java
@@ -0,0 +1,10 @@
+package com.touchdown.perflowbackend.payroll.command.domain.aggregate;
+
+public enum SeverancePayStatus {
+
+    PENDING,
+    CONFIRMED,
+    REJECTED,
+    COMPLETE
+
+}
