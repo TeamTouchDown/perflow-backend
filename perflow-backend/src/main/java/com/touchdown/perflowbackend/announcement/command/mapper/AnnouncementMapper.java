d4a7bd20bd0cd899b8156c7d3a256a99a1ae7719
Younggi Kim <galaxy138742@gmail.com>
2024-12-04 10:43:52 +0900

    feat: #31 부서 엔티티 import 변경

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/announcement/command/mapper/AnnouncementMapper.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/announcement/command/mapper/AnnouncementMapper.java
@@ -2,8 +2,8 @@ package com.touchdown.perflowbackend.announcement.command.mapper;
 
 import com.touchdown.perflowbackend.announcement.command.application.dto.AnnouncementRequestDTO;
 import com.touchdown.perflowbackend.announcement.command.domain.aggregate.Announcement;
-import com.touchdown.perflowbackend.department.command.domain.aggregate.Department;
 import com.touchdown.perflowbackend.employee.command.domain.aggregate.Employee;
+import com.touchdown.perflowbackend.hr.command.domain.aggregate.Department;
 
 public class AnnouncementMapper {
 
