880c4fd362dd33caa3a62b4b77a26d3d6c8e4e67
enking <engur9902@gmail.com>
2024-12-04 11:27:33 +0900

    feat/travel: #27 출장 repository 구현

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/domain/repository/WorkAttitudeTravelCommandRepository.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/domain/repository/WorkAttitudeTravelCommandRepository.java
@@ -1,5 +1,7 @@
 package com.touchdown.perflowbackend.workAttitude.command.domain.repository;
 
+import com.touchdown.perflowbackend.employee.command.domain.aggregate.Employee;
+import com.touchdown.perflowbackend.workAttitude.command.domain.aggregate.Status;
 import com.touchdown.perflowbackend.workAttitude.command.domain.aggregate.Travel;
 
 import java.util.List;
@@ -11,9 +13,9 @@ public interface WorkAttitudeTravelCommandRepository {
     // ID로 Travel 조회
     Optional<Travel> findById(Long id);
     // 특정 상태의 Travel 조회
-    List<Travel> findAllByTravelStatus(String travelStatus);
+    List<Travel> findAllByTravelStatus(Status travelStatus);
     // 특정 사원의 Travel 조회
-    List<Travel> findAllByEmpId(String empId);
+    List<Travel> findAllByEmpId(Employee empId);
     // Travel 삭제
     void deleteById(Long id);
 }
