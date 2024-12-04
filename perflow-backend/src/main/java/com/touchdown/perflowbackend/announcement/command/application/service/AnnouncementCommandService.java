5636dfa587bc7618828ebed5d2b99a71fc32414e
Younggi Kim <galaxy138742@gmail.com>
2024-12-04 10:44:27 +0900

    feat: #31 공지 Service 공지 생성 기능 추가

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/announcement/command/application/service/AnnouncementCommandService.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/announcement/command/application/service/AnnouncementCommandService.java
@@ -1,7 +1,15 @@
 package com.touchdown.perflowbackend.announcement.command.application.service;
 
 import com.touchdown.perflowbackend.announcement.command.application.dto.AnnouncementRequestDTO;
+import com.touchdown.perflowbackend.announcement.command.domain.aggregate.Announcement;
 import com.touchdown.perflowbackend.announcement.command.domain.repository.AnnouncementCommandRepository;
+import com.touchdown.perflowbackend.announcement.command.mapper.AnnouncementMapper;
+import com.touchdown.perflowbackend.common.exception.CustomException;
+import com.touchdown.perflowbackend.common.exception.ErrorCode;
+import com.touchdown.perflowbackend.employee.command.domain.aggregate.Employee;
+import com.touchdown.perflowbackend.employee.command.domain.repository.EmployeeCommandRepository;
+import com.touchdown.perflowbackend.hr.command.domain.aggregate.Department;
+import com.touchdown.perflowbackend.hr.command.domain.repository.DepartmentCommandRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,11 +19,20 @@ import org.springframework.transaction.annotation.Transactional;
 public class AnnouncementCommandService {
 
     private final AnnouncementCommandRepository announcementCommandRepository;
+    private final EmployeeCommandRepository employeeCommandRepository;
+    private final DepartmentCommandRepository departmentCommandRepository;
 
     @Transactional
-    public Long createAnnouncement(AnnouncementRequestDTO announcementRequestDTO) {
+    public void createAnnouncement(AnnouncementRequestDTO announcementRequestDTO) {
 
+        Department foundDepartment = departmentCommandRepository.findById(announcementRequestDTO.getDeptId())
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_DEPARTMENT));
 
-        return null;
+        Employee foundEmployee = employeeCommandRepository.findById(announcementRequestDTO.getEmpId())
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_EMP));
+
+        Announcement newAnnouncement = AnnouncementMapper.toEntity(announcementRequestDTO, foundDepartment, foundEmployee);
+
+        announcementCommandRepository.save(newAnnouncement);
     }
 }
