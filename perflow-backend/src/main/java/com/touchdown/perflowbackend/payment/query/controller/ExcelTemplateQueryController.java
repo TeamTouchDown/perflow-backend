4973e00986a435602308ccb7d982f58ae58a3e58
kimjm9911 <kimjm9911@naver.com>
2024-12-04 15:25:32 +0900

    feat: #23 재직상태와 지급상태 추가

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/payment/query/controller/ExcelTemplateQueryController.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/payment/query/controller/ExcelTemplateQueryController.java
@@ -39,20 +39,22 @@ public class ExcelTemplateQueryController {
         header.createCell(1).setCellValue("이름"); // 직원 이름
         header.createCell(2).setCellValue("직위"); // 직위
         header.createCell(3).setCellValue("부서"); // 부서
-        header.createCell(4).setCellValue("기본급"); // 월급
-        header.createCell(5).setCellValue("연장근무수당"); // 연장 근무 수당
-        header.createCell(6).setCellValue("야간근무수당"); // 야간 근무 수당
-        header.createCell(7).setCellValue("휴일근무수당"); // 휴일 근무 수당
-        header.createCell(8).setCellValue("연차수당"); // 연차 수당
-        header.createCell(9).setCellValue("지급내역합계"); // 지급 내역 합계
-        header.createCell(10).setCellValue("국민연금"); // 국민연금
-        header.createCell(11).setCellValue("건강보험"); // 건강보험
-        header.createCell(12).setCellValue("고용보험"); // 고용보험
-        header.createCell(13).setCellValue("장기요양보험"); // 장기요양보험
-        header.createCell(14).setCellValue("소득세"); // 소득세
-        header.createCell(15).setCellValue("지방소득세"); // 지방소득세
-        header.createCell(16).setCellValue("공제내역합계"); // 공제 내역 합계
-        header.createCell(17).setCellValue("총합계"); // 총 합계
+        header.createCell(4).setCellValue("재직상태"); // 재직상태
+        header.createCell(5).setCellValue("기본급"); // 월급
+        header.createCell(6).setCellValue("연장근무수당"); // 연장 근무 수당
+        header.createCell(7).setCellValue("야간근무수당"); // 야간 근무 수당
+        header.createCell(8).setCellValue("휴일근무수당"); // 휴일 근무 수당
+        header.createCell(9).setCellValue("연차수당"); // 연차 수당
+        header.createCell(10).setCellValue("지급내역합계"); // 지급 내역 합계
+        header.createCell(11).setCellValue("국민연금"); // 국민연금
+        header.createCell(12).setCellValue("건강보험"); // 건강보험
+        header.createCell(13).setCellValue("고용보험"); // 고용보험
+        header.createCell(14).setCellValue("장기요양보험"); // 장기요양보험
+        header.createCell(15).setCellValue("소득세"); // 소득세
+        header.createCell(16).setCellValue("지방소득세"); // 지방소득세
+        header.createCell(17).setCellValue("공제내역합계"); // 공제 내역 합계
+        header.createCell(18).setCellValue("총합계"); // 총 합계
+        header.createCell(19).setCellValue("지급상태"); // 지급 상태
 
 
         // 사원 정보 가져오기 (예시: 사원 목록을 가져옵니다)
@@ -69,21 +71,22 @@ public class ExcelTemplateQueryController {
             row.createCell(2).setCellValue(positionName);  // 직위 이름
             String deptName = employee.getDept().getName();  // 부서 이름을 가져옴
             row.createCell(3).setCellValue(deptName); // 부서 이름
-            row.createCell(4).setCellValue(employee.getPay()); // 월급
-            row.createCell(5);  // 연장 근무 수당 (이후에 계산하여 값 설정)
-            row.createCell(6);  // 야간 근무 수당 (이후에 계산하여 값 설정)
-            row.createCell(7);  // 휴일 근무 수당 (이후에 계산하여 값 설정)
-            row.createCell(8);  // 연차 수당 (이후에 계산하여 값 설정)
-            row.createCell(9);  // 지급 내역 합계 (이후에 계산하여 값 설정)
-            row.createCell(10); // 국민연금 (이후에 계산하여 값 설정)
-            row.createCell(11); // 건강보험 (이후에 계산하여 값 설정)
-            row.createCell(12); // 고용보험 (이후에 계산하여 값 설정)
-            row.createCell(13); // 장기요양보험 (이후에 계산하여 값 설정)
-            row.createCell(14); // 소득세 (이후에 계산하여 값 설정)
-            row.createCell(15); // 지방소득세 (이후에 계산하여 값 설정)
-            row.createCell(16); // 공제 내역 합계 (이후에 계산하여 값 설정)
-            row.createCell(17); // 총 합계 (이후에 계산하여 값 설정)
-
+            row.createCell(4).setCellValue(employee.getStatus().name());
+            row.createCell(5).setCellValue(employee.getPay()); // 월급
+            row.createCell(6);  // 연장 근무 수당 (이후에 계산하여 값 설정)
+            row.createCell(7);  // 야간 근무 수당 (이후에 계산하여 값 설정)
+            row.createCell(8);  // 휴일 근무 수당 (이후에 계산하여 값 설정)
+            row.createCell(9);  // 연차 수당 (이후에 계산하여 값 설정)
+            row.createCell(10); // 지급 내역 합계 (이후에 계산하여 값 설정)
+            row.createCell(11); // 국민연금 (이후에 계산하여 값 설정)
+            row.createCell(12); // 건강보험 (이후에 계산하여 값 설정)
+            row.createCell(13); // 고용보험 (이후에 계산하여 값 설정)
+            row.createCell(14); // 장기요양보험 (이후에 계산하여 값 설정)
+            row.createCell(15); // 소득세 (이후에 계산하여 값 설정)
+            row.createCell(16); // 지방소득세 (이후에 계산하여 값 설정)
+            row.createCell(17); // 공제 내역 합계 (이후에 계산하여 값 설정)
+            row.createCell(18); // 총 합계 (이후에 계산하여 값 설정)
+            row.createCell(19).setCellValue("PENDING"); // 지급 상태(대기)
         }
 
         // 템플릿을 ByteArray로 변환하여 반환
