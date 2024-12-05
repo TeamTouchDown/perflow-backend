1bde457c8a75e7b9fb194e8ffe00fd00cfc6e178
Younggi Kim <galaxy138742@gmail.com>
2024-12-05 10:35:30 +0900

    feat: Swagger 어노테이션 추가 및 API 리턴타입 수정

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/announcement/command/application/controller/AnnouncementCommandController.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/announcement/command/application/controller/AnnouncementCommandController.java
@@ -3,10 +3,12 @@ package com.touchdown.perflowbackend.announcement.command.application.controller
 import com.touchdown.perflowbackend.announcement.command.application.dto.AnnouncementRequestDTO;
 import com.touchdown.perflowbackend.announcement.command.application.service.AnnouncementCommandService;
 import com.touchdown.perflowbackend.common.exception.SuccessCode;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Announcement-Controller", description = "공지 관련 API")
 @RestController
 @RequestMapping("/api/v1/announcements")
 @RequiredArgsConstructor
@@ -15,29 +17,29 @@ public class AnnouncementCommandController {
     private final AnnouncementCommandService announcementCommandService;
 
     @PostMapping
-    public ResponseEntity<String> createAnnouncement(@RequestBody AnnouncementRequestDTO announcementRequestDTO) {
+    public ResponseEntity<SuccessCode> createAnnouncement(@RequestBody AnnouncementRequestDTO announcementRequestDTO) {
 
         announcementCommandService.createAnnouncement(announcementRequestDTO);
 
-        return ResponseEntity.ok(SuccessCode.SUCCESS.getMessage());
+        return ResponseEntity.ok(SuccessCode.SUCCESS);
     }
 
     @PutMapping("/{annId}")
-    public ResponseEntity<String> updateAnnouncement(
+    public ResponseEntity<SuccessCode> updateAnnouncement(
             @PathVariable Long annId,
             @RequestBody AnnouncementRequestDTO announcementRequestDTO) {
 
         announcementCommandService.updateAnnouncement(annId, announcementRequestDTO);
 
-        return ResponseEntity.ok(SuccessCode.SUCCESS.getMessage());
+        return ResponseEntity.ok(SuccessCode.SUCCESS);
     }
 
     @DeleteMapping("/{annId}")
-    public ResponseEntity<String> deleteAnnouncement(@PathVariable Long annId, @RequestBody String empId) {
+    public ResponseEntity<SuccessCode> deleteAnnouncement(@PathVariable Long annId, @RequestBody String empId) {
 
         // empId는 토큰에서 추출하는 로직으로 변경 예정
         announcementCommandService.deleteAnnouncement(annId, empId);
 
-        return ResponseEntity.ok(SuccessCode.SUCCESS.getMessage());
+        return ResponseEntity.ok(SuccessCode.SUCCESS);
     }
 }
