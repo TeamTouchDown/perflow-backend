cdd6d239eb901450411c83357fc21d6b4bf7477e
Younggi Kim <galaxy138742@gmail.com>
2024-12-05 11:11:53 +0900

    feat: #43 entityToDTO 메소드 추가

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/announcement/command/mapper/AnnouncementMapper.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/announcement/command/mapper/AnnouncementMapper.java
@@ -2,6 +2,7 @@ package com.touchdown.perflowbackend.announcement.command.mapper;
 
 import com.touchdown.perflowbackend.announcement.command.application.dto.AnnouncementRequestDTO;
 import com.touchdown.perflowbackend.announcement.command.domain.aggregate.Announcement;
+import com.touchdown.perflowbackend.announcement.query.dto.AnnouncementResponseDTO;
 import com.touchdown.perflowbackend.employee.command.domain.aggregate.Employee;
 import com.touchdown.perflowbackend.hr.command.domain.aggregate.Department;
 
@@ -15,5 +16,17 @@ public class AnnouncementMapper {
                 .title(announcementRequestDTO.getTitle())
                 .content(announcementRequestDTO.getContent())
                 .build();
+    }
+
+    public static AnnouncementResponseDTO toDTO(Announcement announcement) {
+        return new AnnouncementResponseDTO(
+                announcement.getAnnId(),
+                announcement.getDept().getName(),
+                announcement.getEmp().getName(),
+                announcement.getTitle(),
+                announcement.getContent(),
+                announcement.getCreateDatetime(),
+                announcement.getUpdateDatetime()
+        );
     }
 }
