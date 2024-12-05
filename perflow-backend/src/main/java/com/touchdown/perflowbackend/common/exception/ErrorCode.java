880672416f57946cd380a24f52c7d79a94895227
enking <engur9902@gmail.com>
2024-12-05 14:16:17 +0900

    feat: #53 error 코드 추가

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/common/exception/ErrorCode.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/common/exception/ErrorCode.java
@@ -37,7 +37,8 @@ public enum ErrorCode {
     NOT_FOUND_APPROVE_SBJ(HttpStatus.NOT_FOUND, "결재 주체 정보를 찾을 수 없습니다."),
     INVALID_APPROVE_SBJ_ID(HttpStatus.BAD_REQUEST, "유효하지 않은 결재 주체 ID입니다."),
 
-    NOT_FOUND_ANNOUNCEMENT(HttpStatus.NOT_FOUND, "해당 공지사항을 찾을 수 없습니다.");
+    NOT_FOUND_ANNOUNCEMENT(HttpStatus.NOT_FOUND, "해당 공지사항을 찾을 수 없습니다."),
+    NOT_FOUND_TRAVEL(HttpStatus.NOT_FOUND, "해당 내역을 찾을 수 없습니다.");
 
 
     private final HttpStatus httpStatus;
