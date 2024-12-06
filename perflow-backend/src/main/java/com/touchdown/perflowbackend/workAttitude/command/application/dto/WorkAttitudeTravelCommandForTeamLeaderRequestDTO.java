be77685abc444812a54bf6785b3a3e3286fe0821
enking <engur9902@gmail.com>
2024-12-06 15:49:40 +0900

    feat: #53 teamLeader 용  DTO 제작

---
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/application/dto/WorkAttitudeTravelCommandForTeamLeaderRequestDTO.java
@@ -0,0 +1,12 @@
+package com.touchdown.perflowbackend.workAttitude.command.application.dto;
+
+import jakarta.annotation.Nullable;
+import lombok.Data;
+
+@Data
+public class WorkAttitudeTravelCommandForTeamLeaderRequestDTO {
+    private String travelStatus;
+
+    @Nullable
+    private String rejectReason;
+}
