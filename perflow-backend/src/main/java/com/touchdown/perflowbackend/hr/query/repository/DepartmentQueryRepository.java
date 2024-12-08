8faee1b706387ad82d6c1afbc3b26161077731d4
eunseo76 <eunseolee19@gmail.com>
2024-12-08 15:57:52 +0900

    feat/hr: #81 부서 트리 조회 구현

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/hr/query/repository/DepartmentQueryRepository.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/hr/query/repository/DepartmentQueryRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DepartmentQueryRepository extends JpaRepository<Department, Long> {
 
     @Query("SELECT d FROM Department d")
-    List<Department> findAllDepartments();
+    Optional<List<Department>> findAllDepts();
 }
--- perflow-backend/src/main/java/com/touchdown/perflowbackend/hr/query/service/HrQueryService.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/hr/query/service/HrQueryService.java
@@ -1,5 +1,7 @@
 package com.touchdown.perflowbackend.hr.query.service;
 
+import com.touchdown.perflowbackend.common.exception.CustomException;
+import com.touchdown.perflowbackend.common.exception.ErrorCode;
 import com.touchdown.perflowbackend.hr.command.domain.aggregate.Department;
 import com.touchdown.perflowbackend.hr.query.dto.DepartmentQueryResponse;
 import com.touchdown.perflowbackend.hr.query.repository.DepartmentQueryRepository;
@@ -7,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,27 +22,39 @@ public class HrQueryService {
 
     public List<DepartmentQueryResponse> readAllDepartments() {
 
-        List<Department> allDepartments = departmentQueryRepository.findAllDepartments();
+        List<Department> allDepartments = findAllDepartments();
 
-        // 상위 부서가 없는 최상위 부서 조회
-        List<Department> topLevelDepartments = allDepartments.stream()
-                .filter(department -> department.getManageDept() == null)
-                .toList();
+        List<Department> topLevelDepartments = findTopLevelDepartments(allDepartments);
 
         return topLevelDepartments.stream()
                 .map(department -> buildDepartmentTree(department, allDepartments))
                 .collect(Collectors.toList());
     }
-    
+
+    // 모든 부서 탐색
+    private List<Department> findAllDepartments() {
+
+        return departmentQueryRepository.findAllDepts()
+                .orElse(Collections.emptyList());
+    }
+
+    // 상위 부서가 없는 최상위 부서 탐색
+    private List<Department> findTopLevelDepartments(List<Department> allDepartments) {
+
+        return allDepartments.stream()
+                .filter(department -> department.getManageDept() == null)
+                .toList();
+    }
+
+    // 부서 트리 구조 만들기
     private DepartmentQueryResponse buildDepartmentTree(Department department, List<Department> allDepartments) {
 
-        // 현재 부서의 자식 부서들
+        // 현재 부서의 자식 부서 탐색
         List<DepartmentQueryResponse> subDepartments = allDepartments.stream()
                 .filter(subDept -> department.equals(subDept.getManageDept()))
                 .map(subDept -> buildDepartmentTree(subDept, allDepartments))
                 .collect(Collectors.toList());
 
-        // 현재 부서와 자식 부서를 응답 DTO로 변환
         return new DepartmentQueryResponse(department.getDepartmentId(), department.getName(), subDepartments);
     }
 
