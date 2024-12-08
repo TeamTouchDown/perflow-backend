8e02ad3800e9340fdf50d653ca8849f8023b4e4c
enking <engur9902@gmail.com>
2024-12-08 23:55:12 +0900

    feat: #79 overtime Command Controller & Service

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/application/controller/WorkAttributeOvertimeCommandController.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/application/controller/WorkAttributeOvertimeCommandController.java
@@ -2,6 +2,8 @@ package com.touchdown.perflowbackend.workAttitude.command.application.controller
 
 import com.touchdown.perflowbackend.common.exception.SuccessCode;
 import com.touchdown.perflowbackend.workAttitude.command.application.service.WorkAttributeOvertimeCommandService;
+
+import com.touchdown.perflowbackend.workAttitude.command.application.dto.WorkAttributeOvertimeForEmployeeRequestDTO;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,16 +18,16 @@ public class WorkAttributeOvertimeCommandController {
 
     // 직원: 초과근무 등록
     @PostMapping("/emp/overtimes")
-    public ResponseEntity<SuccessCode> createOvertime(@RequestBody WorkAttributeOvertimeForEmployeeRequestDTO requestDTO) {
-        workAttributeOvertimeCommandService.createOvertime(requestDTO);
+    public ResponseEntity<SuccessCode> createOvertime(@RequestBody WorkAttributeOvertimeForEmployeeRequestDTO workAttributeOvertimeForEmployeeRequestDTO) {
+        workAttributeOvertimeCommandService.createOvertime(workAttributeOvertimeForEmployeeRequestDTO);
         return ResponseEntity.ok(SuccessCode.SUCCESS);
     }
 
     // 직원: 초과근무 수정
     @PutMapping("/emp/{overtimeId}")
     public ResponseEntity<SuccessCode> updateOvertime(@PathVariable Long overtimeId,
-                                                      @RequestBody WorkAttributeOvertimeRequestDTO requestDTO) {
-        workAttributeOvertimeCommandService.updateOvertime(overtimeId, requestDTO);
+                                                      @RequestBody WorkAttributeOvertimeForEmployeeRequestDTO workAttributeOvertimeForEmployeeRequestDTO) {
+        workAttributeOvertimeCommandService.updateOvertime(overtimeId, workAttributeOvertimeForEmployeeRequestDTO);
         return ResponseEntity.ok(SuccessCode.SUCCESS);
     }
 
@@ -39,8 +41,8 @@ public class WorkAttributeOvertimeCommandController {
     // 팀장: 초과근무 상태 변경 (승인/반려)
     @PutMapping("/leader/{overtimeId}/status")
     public ResponseEntity<SuccessCode> updateOvertimeStatus(@PathVariable Long overtimeId,
-                                                            @RequestBody WorkAttributeOvertimeStatusChangeDTO statusChangeDTO) {
-        workAttributeOvertimeCommandService.updateOvertimeStatus(overtimeId, statusChangeDTO);
+                                                            @RequestBody WorkAttributeOvertimeForEmployeeRequestDTO workAttributeOvertimeForEmployeeRequestDTO) {
+        workAttributeOvertimeCommandService.updateOvertimeStatus(overtimeId, workAttributeOvertimeForEmployeeRequestDTO);
         return ResponseEntity.ok(SuccessCode.SUCCESS);
     }
 
--- perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/application/service/WorkAttributeOvertimeCommandService.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/application/service/WorkAttributeOvertimeCommandService.java
@@ -1,9 +1,21 @@
 package com.touchdown.perflowbackend.workAttitude.command.application.service;
 
+import com.touchdown.perflowbackend.workAttitude.command.application.dto.WorkAttributeOvertimeForEmployeeRequestDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class WorkAttributeOvertimeCommandService {
+    public void createOvertime(WorkAttributeOvertimeForEmployeeRequestDTO workAttributeOvertimeForEmployeeRequestDTO) {
+    }
+
+    public void updateOvertime(Long overtimeId, WorkAttributeOvertimeForEmployeeRequestDTO workAttributeOvertimeForEmployeeRequestDTO) {
+    }
+
+    public void deleteOvertime(Long overtimeId) {
+    }
+
+    public void updateOvertimeStatus(Long overtimeId, WorkAttributeOvertimeForEmployeeRequestDTO workAttributeOvertimeForEmployeeRequestDTO) {
+    }
 }
