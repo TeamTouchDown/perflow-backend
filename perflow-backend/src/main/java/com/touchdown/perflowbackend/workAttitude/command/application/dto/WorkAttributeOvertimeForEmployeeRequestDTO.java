b984c8be72a5a65385a097da6f146273827a3ed1
enking <engur9902@gmail.com>
2024-12-08 20:49:16 +0900

    feat: #79 command 용 DTO

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/application/dto/WorkAttributeOvertimeForEmployeeRequestDTO.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/application/dto/WorkAttributeOvertimeForEmployeeRequestDTO.java
@@ -1,4 +1,30 @@
 package com.touchdown.perflowbackend.workAttitude.command.application.dto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class WorkAttributeOvertimeForEmployeeRequestDTO {
+
+    @NotNull
+    private String empId;
+
+    @NotNull
+    private Long approveSbjId;
+
+    @NotNull
+    private String overtimeType;
+
+    @NotNull
+    private LocalDateTime overtimeStart;
+
+    @NotNull
+    private LocalDateTime overtimeEnd;
+
+    private Boolean isOvertimeRetroactive;
+
+    private String overtimeRetroactiveReason;
+
 }
