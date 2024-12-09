b022b9ddcc2a7ef36f675eac79ec35c358cd604f
enking <engur9902@gmail.com>
2024-12-09 11:23:14 +0900

    feat: #79 overtime Command service & 레파지토리

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/application/service/WorkAttributeOvertimeCommandService.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/application/service/WorkAttributeOvertimeCommandService.java
@@ -1,12 +1,20 @@
 package com.touchdown.perflowbackend.workAttitude.command.application.service;
 
+import com.touchdown.perflowbackend.approval.command.domain.repository.ApproveCommandRepository;
+import com.touchdown.perflowbackend.employee.command.domain.repository.EmployeeCommandRepository;
 import com.touchdown.perflowbackend.workAttitude.command.application.dto.WorkAttributeOvertimeForEmployeeRequestDTO;
+import com.touchdown.perflowbackend.workAttitude.command.domain.repository.WorkAttributeOvertimeCommandRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class WorkAttributeOvertimeCommandService {
+
+    private final WorkAttributeOvertimeCommandRepository workAttributeOvertimeCommandRepository;
+    private final EmployeeCommandRepository employeeCommandRepository; // string 값
+    private final ApproveCommandRepository approveCommandRepository;
+
     public void createOvertime(WorkAttributeOvertimeForEmployeeRequestDTO workAttributeOvertimeForEmployeeRequestDTO) {
     }
 
