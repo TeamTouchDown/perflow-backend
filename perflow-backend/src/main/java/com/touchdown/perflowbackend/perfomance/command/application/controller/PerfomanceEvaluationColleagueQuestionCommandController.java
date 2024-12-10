a0f00200b443af9ef8bec8983d199f55dd971ace
blazhech <132972216+HanDJ00@users.noreply.github.com>
2024-12-10 11:19:21 +0900

    Update PerfomanceEvaluationColleagueQuestionCommandController.java

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/perfomance/command/application/controller/PerfomanceEvaluationColleagueQuestionCommandController.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/perfomance/command/application/controller/PerfomanceEvaluationColleagueQuestionCommandController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/hr/perfomances/col/perfo/question/")
+@RequestMapping("/api/v1/hr/perfomances/col/perfo/question")
 @RequiredArgsConstructor
 public class PerfomanceEvaluationColleagueQuestionCommandController {
 
