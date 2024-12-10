d37506760b12387b03c1ed176dace03dcdbe0132
blazhech <132972216+HanDJ00@users.noreply.github.com>
2024-12-10 11:22:15 +0900

    Update PerfomanceEvaluationCollagueQuestionQueryController.java

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/perfomance/query/controller/PerfomanceEvaluationCollagueQuestionQueryController.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/perfomance/query/controller/PerfomanceEvaluationCollagueQuestionQueryController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/hr/perfomances/col/perfo/question/")
+@RequestMapping("/api/v1/hr/perfomances/col/perfo/question")
 @RequiredArgsConstructor
 public class PerfomanceEvaluationCollagueQuestionQueryController {
 
