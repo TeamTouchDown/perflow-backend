21631a0586969f5561d88516ab81e49f72f044d8
enking <engur9902@gmail.com>
2024-12-11 11:00:50 +0900

    feat: #106 DTO 구성 수정

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/query/dto/WorkAttributeOvertimeForTeamLeaderSummaryDTO.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/query/dto/WorkAttributeOvertimeForTeamLeaderSummaryDTO.java
@@ -5,9 +5,15 @@ import lombok.Data;
 @Data
 @Builder
 public class WorkAttributeOvertimeForTeamLeaderSummaryDTO {
+
+
     private String employeeName; // 사원 이름
+
     private long nightHours; // 야간 근무 시간
+
     private long holidayHours; // 휴일 근무 시간
+
     private long extendedHours; // 연장 근무 시간
+
     private long totalHours; // 총 근무 시간
 }
