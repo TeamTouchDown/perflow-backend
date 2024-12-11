bfed0541d454d4f463761b6252083b209fccc524
kimjm9911 <kimjm9911@naver.com>
2024-12-11 10:39:40 +0900

    chore: #103 사원 목록 조회 query로 이동

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/employee/query/repository/EmployeeQueryRepository.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/employee/query/repository/EmployeeQueryRepository.java
@@ -1,6 +1,7 @@
 package com.touchdown.perflowbackend.employee.query.repository;
 
 import com.touchdown.perflowbackend.employee.command.domain.aggregate.Employee;
+import com.touchdown.perflowbackend.payment.query.dto.PayrollDTO;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,4 +12,7 @@ public interface EmployeeQueryRepository extends JpaRepository<Employee, Long> {
 
     @Query("SELECT e FROM Employee e WHERE e.dept.departmentId = :departmentId")
     List<Employee> findByDeptId(@Param("deptId") Long departmentId);
+
+    List<Employee> findAll(); // 사원 목록 조회
+
 }
