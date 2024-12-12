030a63105b07bcd0db37a1f3dff3aaecbbea18a9
enking <engur9902@gmail.com>
2024-12-12 16:17:33 +0900

    feat:#106 dto mapper 구현

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/domain/aggregate/Attendance.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/domain/aggregate/Attendance.java
@@ -3,6 +3,7 @@ package com.touchdown.perflowbackend.workAttitude.command.domain.aggregate;
 import com.touchdown.perflowbackend.employee.command.domain.aggregate.Employee;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -24,14 +25,23 @@ public class Attendance { //출퇴
     private Employee empId;
 
     @Column(name = "get_work_datetime", nullable = true)
-    private LocalDateTime getWorkDatetime;
+    private LocalDateTime checkInDateTime;
 
     @Column(name = "get_off_datetime", nullable = true) // 퇴근 시간이 없을 수도 있음
-    private LocalDateTime getOffDatetime;
+    private LocalDateTime checkOutDateTime;
 
     @Column(name = "status", nullable = false, length = 10)
     private String status; // 상태: "출근" / "퇴근"
 
 
-
+    @Builder
+    public Attendance(Employee empId,
+                      LocalDateTime CheckInDateTime,
+                      LocalDateTime CheckOutDateTime,
+                      String status) {
+        this.empId = empId;
+        this.checkInDateTime = CheckInDateTime;
+        this.checkOutDateTime = CheckOutDateTime;
+        this.status = status;
+    }
 }
--- perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/mapper/WorkAttitudeAttendanceMapper.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/mapper/WorkAttitudeAttendanceMapper.java
@@ -1,4 +1,29 @@
 package com.touchdown.perflowbackend.workAttitude.command.mapper;
 
+import com.touchdown.perflowbackend.employee.command.domain.aggregate.Employee;
+import com.touchdown.perflowbackend.workAttitude.command.application.dto.WorkAttitudeAttendanceRequestDTO;
+import com.touchdown.perflowbackend.workAttitude.command.domain.aggregate.Attendance;
+import com.touchdown.perflowbackend.workAttitude.command.domain.aggregate.Status;
+import com.touchdown.perflowbackend.workAttitude.query.dto.WorkAttitudeAttendanceResponseDTO;
+
 public class WorkAttitudeAttendanceMapper {
+
+    public static Attendance toEntity(WorkAttitudeAttendanceRequestDTO requestDTO, Employee employee){
+        return Attendance.builder()
+                .empId(employee)
+                .CheckInDateTime(requestDTO.getCheckInDateTime())
+                .CheckOutDateTime(requestDTO.getCheckOutDateTime())
+                .status(requestDTO.getStatus().toString())
+                .build();
+    }
+
+    public static WorkAttitudeAttendanceResponseDTO toResponseDTO(Attendance attendance) {
+        return WorkAttitudeAttendanceResponseDTO.builder()
+                .attendanceId(attendance.getAttendanceId())
+                .empId(attendance.getEmpId().getEmpId())
+                .checkInDateTime(attendance.getCheckInDateTime())
+                .checkOutDateTime(attendance.getCheckOutDateTime())
+                .status(Status.valueOf(attendance.getStatus())) // String -> Enum 변환
+                .build();
+    }
 }
