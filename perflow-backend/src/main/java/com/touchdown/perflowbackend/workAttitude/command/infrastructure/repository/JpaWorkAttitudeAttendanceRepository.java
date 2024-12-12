9517e4b82d10b20a1720306187927bfa9d40d295
enking <engur9902@gmail.com>
2024-12-12 16:19:14 +0900

    feat:#106 repository 구현

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/infrastructure/repository/JpaWorkAttitudeAttendanceRepository.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/infrastructure/repository/JpaWorkAttitudeAttendanceRepository.java
@@ -1,4 +1,8 @@
 package com.touchdown.perflowbackend.workAttitude.command.infrastructure.repository;
 
-public interface JpaWorkAttitudeAttendanceRepository {
+import com.touchdown.perflowbackend.workAttitude.command.domain.aggregate.Attendance;
+import com.touchdown.perflowbackend.workAttitude.command.domain.repository.WorkAttitudeAttendanceCommandRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaWorkAttitudeAttendanceRepository extends WorkAttitudeAttendanceCommandRepository, JpaRepository<Attendance, Long> {
 }
