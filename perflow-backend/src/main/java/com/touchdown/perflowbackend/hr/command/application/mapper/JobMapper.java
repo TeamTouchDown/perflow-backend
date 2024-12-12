8cd1c5f7a5de32e614ec2e7ca9b5cf639c33fa81
JungUiJin <yealkki38@gmail.com>
2024-12-12 15:37:27 +0900

    feat: #118 직위 전체 조회 기능 구현

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/hr/command/application/mapper/JobMapper.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/hr/command/application/mapper/JobMapper.java
@@ -3,6 +3,10 @@ package com.touchdown.perflowbackend.hr.command.application.mapper;
 import com.touchdown.perflowbackend.hr.command.application.dto.job.JobCreateDTO;
 import com.touchdown.perflowbackend.hr.command.domain.aggregate.Department;
 import com.touchdown.perflowbackend.hr.command.domain.aggregate.Job;
+import com.touchdown.perflowbackend.hr.query.dto.JobResponseDTO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JobMapper {
 
@@ -12,5 +16,20 @@ public class JobMapper {
                 .createDTO(jobCreateDTO)
                 .dept(department)
                 .build();
+    }
+
+    public static List<JobResponseDTO> toJobResponseDTOList(List<Job> jobs) {
+
+        List<JobResponseDTO> jobResponseDTOList = new ArrayList<>();
+
+        for (Job job : jobs) {
+            jobResponseDTOList.add(
+                    JobResponseDTO.builder()
+                            .job(job)
+                            .build()
+            );
+        }
+
+        return jobResponseDTOList;
     }
 }
