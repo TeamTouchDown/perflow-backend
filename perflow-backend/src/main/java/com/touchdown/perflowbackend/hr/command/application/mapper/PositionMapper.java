26d03d018c4fe95f11175bc112756ebe64b73f30
JungUiJin <yealkki38@gmail.com>
2024-12-12 12:11:45 +0900

    feat: #114 직위 전체 조회 기능 구현

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/hr/command/application/mapper/PositionMapper.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/hr/command/application/mapper/PositionMapper.java
@@ -2,6 +2,10 @@ package com.touchdown.perflowbackend.hr.command.application.mapper;
 
 import com.touchdown.perflowbackend.hr.command.application.dto.position.PositionCreateDTO;
 import com.touchdown.perflowbackend.hr.command.domain.aggregate.Position;
+import com.touchdown.perflowbackend.hr.query.dto.PositionResponseDTO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PositionMapper {
 
@@ -10,5 +14,21 @@ public class PositionMapper {
         return Position.builder()
                 .positionCreateDTO(positionCreateDTO)
                 .build();
+    }
+
+    public static List<PositionResponseDTO> toResponse(List<Position> positions) {
+
+        List<PositionResponseDTO> positionResponseDTOS = new ArrayList<>();
+
+        for (Position position : positions) {
+            positionResponseDTOS.add(PositionResponseDTO.builder()
+                    .positionId(position.getPositionId())
+                    .name(position.getName())
+                    .positionLevel(position.getPositionLevel())
+                    .status(position.getStatus())
+                    .build());
+        }
+
+        return positionResponseDTOS;
     }
 }
---
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/hr/query/controller/PositionQueryController.java
@@ -0,0 +1,27 @@
+package com.touchdown.perflowbackend.hr.query.controller;
+
+import com.touchdown.perflowbackend.hr.query.dto.PositionResponseDTO;
+import com.touchdown.perflowbackend.hr.query.service.PositionQueryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class PositionQueryController {
+
+    private final PositionQueryService positionQueryService;
+
+    @GetMapping("/position")
+    public ResponseEntity<List<PositionResponseDTO>> getPosition() {
+
+        List<PositionResponseDTO> positionDTOList = positionQueryService.getAllPosition();
+        
+        return ResponseEntity.ok(positionDTOList);
+    }
+}
