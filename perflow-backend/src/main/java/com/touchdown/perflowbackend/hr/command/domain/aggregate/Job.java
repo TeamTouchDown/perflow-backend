e7378be82a48d52d34549a732dcd91b60344f77a
JungUiJin <yealkki38@gmail.com>
2024-12-12 14:43:20 +0900

    feat: #118 직위 정보 등록 구현

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/hr/command/domain/aggregate/Job.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/hr/command/domain/aggregate/Job.java
@@ -1,14 +1,18 @@
 package com.touchdown.perflowbackend.hr.command.domain.aggregate;
 
 import com.touchdown.perflowbackend.common.BaseEntity;
+import com.touchdown.perflowbackend.hr.command.application.dto.job.JobCreateDTO;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @Entity
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "job", schema = "perflow")
 public class Job extends BaseEntity {
 
@@ -26,10 +30,18 @@ public class Job extends BaseEntity {
     @Column(name = "responsibility", nullable = false)
     private String responsibility;
 
-    @Column(name = "is_active", nullable = false)
-    private Boolean isActive;
-
     @Column(name = "status", nullable = false)
-    private String status;
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Builder
+    public Job(JobCreateDTO createDTO, Department dept) {
+
+        this.jobId = createDTO.getJobId();
+        this.dept = dept;
+        this.name = createDTO.getName();
+        this.responsibility = createDTO.getResponsibility();
+        this.status = createDTO.getStatus();
+    }
 
 }
