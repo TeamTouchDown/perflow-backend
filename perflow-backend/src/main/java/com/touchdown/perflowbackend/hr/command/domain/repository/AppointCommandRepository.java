dca72082ca73f2aa713fe1d93da96aace1d75a1e
JungUiJin <yealkki38@gmail.com>
2024-12-13 15:51:20 +0900

    feat: #125 직책 변경 기능 구현

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/hr/command/domain/repository/AppointCommandRepository.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/hr/command/domain/repository/AppointCommandRepository.java
@@ -1,4 +1,8 @@
 package com.touchdown.perflowbackend.hr.command.domain.repository;
 
+import com.touchdown.perflowbackend.hr.command.domain.aggregate.Appoint;
+
 public interface AppointCommandRepository {
+
+    Appoint save(Appoint appoint);
 }
