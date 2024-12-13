637ce7c0ee61e2dc0522278e73c6cd608dd62755
enking <engur9902@gmail.com>
2024-12-13 17:22:30 +0900

    feat:#106 출퇴근 command 부 구현

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/domain/repository/WorkAttitudeAttendanceCommandRepository.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/domain/repository/WorkAttitudeAttendanceCommandRepository.java
@@ -1,4 +1,8 @@
 package com.touchdown.perflowbackend.workAttitude.command.domain.repository;
 
+import com.touchdown.perflowbackend.workAttitude.command.domain.aggregate.Attendance;
+
 public interface WorkAttitudeAttendanceCommandRepository {
+
+    Attendance save(Attendance attendance);
 }
