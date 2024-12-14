0dc2f12ccc3401b56d0adbdfe8dad7772001395a
enking <engur9902@gmail.com>
2024-12-14 20:28:19 +0900

    feat:#106 출장 swagger 설명 추ㅏㄱ

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/application/controller/WorkAttitudeTravelCommandController.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/application/controller/WorkAttitudeTravelCommandController.java
@@ -4,11 +4,13 @@ import com.touchdown.perflowbackend.common.exception.SuccessCode;
 import com.touchdown.perflowbackend.workAttitude.command.application.dto.WorkAttitudeTravelCommandForTeamLeaderRequestDTO;
 import com.touchdown.perflowbackend.workAttitude.command.application.dto.WorkAttitudeTravelRequestDTO;
 import com.touchdown.perflowbackend.workAttitude.command.application.service.WorkAttitudeTravelCommandService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-@Tag(name = "WorkAttribute-Controller", description = "출장 관련 API")
+
+@Tag(name = "WorkAttitude-Controller", description = "출장 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
@@ -16,15 +18,15 @@ public class WorkAttitudeTravelCommandController {
 
     private final WorkAttitudeTravelCommandService workAttitudeTravelCommandService;
 
-    //출장 등록(사원)
+    @Operation(summary = "사원이 출장 등록", description = "사원이 자신의 출장 요청을 등록")
     @PostMapping("/emp/travels")
-    public ResponseEntity<SuccessCode>requestTravel(
-            @RequestBody WorkAttitudeTravelRequestDTO workAttitudeTravelRequestDTO){
+    public ResponseEntity<SuccessCode> requestTravel(
+            @RequestBody WorkAttitudeTravelRequestDTO workAttitudeTravelRequestDTO) {
         workAttitudeTravelCommandService.createTravel(workAttitudeTravelRequestDTO);
-        return ResponseEntity.ok(SuccessCode.WORK_ATTITUDE_TRAVEL_SUCCESS) ;
+        return ResponseEntity.ok(SuccessCode.WORK_ATTITUDE_TRAVEL_SUCCESS);
     }
 
-    // 3. 출장 수정 (사원)
+    @Operation(summary = "사원이 출장 수정", description = "사원이 자신의 출장 요청을 수정")
     @PutMapping("/emp/travel/{travelId}")
     public ResponseEntity<SuccessCode> updateTravel(
             @PathVariable Long travelId,
@@ -32,36 +34,29 @@ public class WorkAttitudeTravelCommandController {
         workAttitudeTravelCommandService.updateTravel(travelId, workAttitudeTravelRequestDTO);
         return ResponseEntity.ok(SuccessCode.SUCCESS);
     }
-    // 4. 출장 삭제 (사원)
+
+    @Operation(summary = "사원이 출장 삭제", description = "사원이 자신의 출장 요청을 삭제")
     @DeleteMapping("/emp/travel/{travelId}")
     public ResponseEntity<SuccessCode> deleteTravel(
-            @PathVariable(name="travelId") Long travelId) {
+            @PathVariable(name = "travelId") Long travelId) {
         workAttitudeTravelCommandService.deleteTravel(travelId);
         return ResponseEntity.ok(SuccessCode.SUCCESS);
     }
-    //------ 팀장 부분 ------
-    // 팀장: 출장 상태 변경 (승인/반려)
+
+    @Operation(summary = "팀장이 출장 상태 변경", description = "팀장이 사원의 출장 요청 상태를 승인, 반려함 반려시 반려사유도 작성 가능")
     @PutMapping("/leader/travel/{travelId}")
     public ResponseEntity<SuccessCode> updateTravelStatus(
-            @PathVariable(name="travelId") Long travelId,
+            @PathVariable(name = "travelId") Long travelId,
             @RequestBody WorkAttitudeTravelCommandForTeamLeaderRequestDTO requestDTO) {
         workAttitudeTravelCommandService.updateTravelStatus(travelId, requestDTO);
         return ResponseEntity.ok(SuccessCode.SUCCESS);
     }
 
-    // 팀장: 출장 삭제
+    @Operation(summary = "팀장이 출장 삭제", description = "팀장이 사원의 출장 요청을 삭제")
     @DeleteMapping("/leader/travel/{travelId}")
     public ResponseEntity<SuccessCode> deleteEmployeeTravel(
-            @PathVariable(name="travelId") Long travelId) {
+            @PathVariable(name = "travelId") Long travelId) {
         workAttitudeTravelCommandService.deleteTravel(travelId);
         return ResponseEntity.ok(SuccessCode.SUCCESS);
     }
-
-
-
-
-
-
-
-
 }
