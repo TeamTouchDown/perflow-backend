d1397875a2a9dcfec4734cdfd2c79ece1d2f2ce5
JungUiJin <yealkki38@gmail.com>
2024-12-17 14:25:39 +0900

    feat: #152 사원 목록 조회 페이지네이션 구현

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/employee/query/controller/EmployeeQueryController.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/employee/query/controller/EmployeeQueryController.java
@@ -2,9 +2,12 @@ package com.touchdown.perflowbackend.employee.query.controller;
 
 import com.touchdown.perflowbackend.employee.query.dto.EmployeeDetailResponseDTO;
 import com.touchdown.perflowbackend.employee.query.dto.EmployeeQueryResponseDTO;
+import com.touchdown.perflowbackend.employee.query.dto.EmployeeResponseList;
 import com.touchdown.perflowbackend.employee.query.service.EmployeeQueryService;
 import com.touchdown.perflowbackend.security.util.EmployeeUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,12 +29,17 @@ public class EmployeeQueryController {
 
     // 모든 사원 목록 조회
     @GetMapping("/employees/lists")
-    public ResponseEntity<List<EmployeeQueryResponseDTO>> getEmployees() {
+    public ResponseEntity<EmployeeResponseList> getEmployees(
+            @RequestParam(value = "page", defaultValue = "1") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size
+    ) {
 
-        return ResponseEntity.ok(employeeQueryService.getAllEmployees());
+        Pageable pageable = PageRequest.of(page - 1, size);
+
+        return ResponseEntity.ok(employeeQueryService.getAllEmployees(pageable));
     }
 
-    @GetMapping("/hr/employees/detail/{empId}") // 관리자의 사원 정보 조회
+    @GetMapping("/hr/employees/{empId}") // 관리자의 사원 정보 조회
     public ResponseEntity<EmployeeDetailResponseDTO> getEmployeeDetail(
             @PathVariable(value = "empId") String empId
     ) {
@@ -39,7 +47,7 @@ public class EmployeeQueryController {
         return ResponseEntity.ok(employeeQueryService.getEmployeeDetail(empId));
     }
 
-    @GetMapping("/employees/detail") // 내 상세정보 조회
+    @GetMapping("/employees") // 내 상세정보 조회
     public ResponseEntity<EmployeeDetailResponseDTO> getMyDetail() {
 
         String empId = EmployeeUtil.getEmpId();
