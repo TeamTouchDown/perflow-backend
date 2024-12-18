6c5b0d308620a6ec33d8f67b7cb18578bf769d9f
HanDJ00 <han06225@gmail.com>
2024-12-18 17:15:02 +0900

    feat: #171 KPI 처리 생성 기능 구현

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/perfomance/command/application/controller/PerfomanceKPIPassCommandController.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/perfomance/command/application/controller/PerfomanceKPIPassCommandController.java
@@ -2,27 +2,27 @@ package com.touchdown.perflowbackend.perfomance.command.application.controller;
 
 import com.touchdown.perflowbackend.common.exception.SuccessCode;
 import com.touchdown.perflowbackend.perfomance.command.application.dto.CreateKpiPassDTO;
+import com.touchdown.perflowbackend.perfomance.command.application.dto.CreateKpiProgressDTO;
 import com.touchdown.perflowbackend.perfomance.command.application.service.KPICommandService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/hr/perfomances/kpi")
+@RequestMapping("/api/v1/hr/perfomances/kpi/pass")
 @RequiredArgsConstructor
-public class PerformanceKPIPassCommandController {
+public class PerfomanceKPIPassCommandController {
 
     private final KPICommandService kpiCommandService;
 
-    // KPI 최신화 생성
-    @PostMapping("/pass/{empId}/{kpiId}")
-    public ResponseEntity<SuccessCode> createKpiProgress(
+    @PostMapping("/{empId}/{kpiId}")
+    public ResponseEntity<SuccessCode> createKpiPass(
             @PathVariable("empId") String empId,
             @PathVariable("kpiId") Long kpiId,
             @RequestBody CreateKpiPassDTO createKpiPassDTO) {
 
-        kpiCommandService.createKpiProgress(empId, kpiId, createKpiPassDTO);
+        kpiCommandService.createKpiPass(empId, kpiId, createKpiPassDTO);
 
-        return ResponseEntity.ok(SuccessCode.KPI_PROGRESS_UPLOAD_SUCCESS);
+        return ResponseEntity.ok(SuccessCode.KPI_PASS_UPLOAD_SUCCESS);
     }
 }
