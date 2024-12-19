7afb7d79be12e98a0712b435e6f962d50ee54661
Younggi Kim <galaxy138742@gmail.com>
2024-12-19 15:27:42 +0900

    feat: #181 공지사항 조회 기능 수정 (작성자 검색 기능 삭제, 부서별 필터링 기능 추가)

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/announcement/query/repository/AnnouncementQueryRepository.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/announcement/query/repository/AnnouncementQueryRepository.java
@@ -4,15 +4,10 @@ import com.touchdown.perflowbackend.announcement.command.domain.aggregate.Announ
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface AnnouncementQueryRepository extends JpaRepository<Announcement, Long> {
 
     Page<Announcement> findByTitleContainingIgnoreCase(String title, Pageable pageable);
 
-    @Query("SELECT a FROM Announcement a " +
-            "JOIN a.emp e " +
-            "WHERE LOWER(e.name) LIKE LOWER(CONCAT('%', :empName, '%'))")
-    Page<Announcement> findByEmpName(@Param("empName") String empName, Pageable pageable);
+    Page<Announcement> findByDeptName(String deptName, Pageable pageable);
 }
