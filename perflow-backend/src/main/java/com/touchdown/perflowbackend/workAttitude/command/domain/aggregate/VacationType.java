3e46f45fad378d25f20647fb2de92dbee799fe17
enking <engur9902@gmail.com>
2024-12-23 19:33:56 +0900

    feat/attitude/#196 entity 수정

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/domain/aggregate/VacationType.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/domain/aggregate/VacationType.java
@@ -1,8 +1,9 @@
 package com.touchdown.perflowbackend.workAttitude.command.domain.aggregate;
 
 public enum VacationType {
-    MATERNITY,
-    SPOUSEMATERNITY,
-    MENSTRUAL,
-    FAMILYCARE
+
+    MATERNITY, // 출산
+    SPOUSEMATERNITY,  //배우자 출산
+    MENSTRUAL, // 생리 휴가
+    FAMILYCARE // 가족 돌봄
 }
