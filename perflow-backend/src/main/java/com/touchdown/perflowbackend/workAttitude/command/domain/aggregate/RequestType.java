f9d8e144486793d54eab3f40ac5f024a6d3a21f5
enking <engur9902@gmail.com>
2024-12-26 22:19:41 +0900

    feat/attitude/#218 table 추가

---
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/domain/aggregate/RequestType.java
@@ -0,0 +1,8 @@
+package com.touchdown.perflowbackend.workAttitude.command.domain.aggregate;
+
+public enum RequestType {
+    ANNUAL,      // 연차
+    VACATION,    // 휴가
+    OVERTIME,    // 초과근무
+    TRAVEL       // 출장
+}
