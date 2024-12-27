93cc5d72f73c8cde0845029c9da4a9c2639d2e0e
enking <engur9902@gmail.com>
2024-12-27 11:14:29 +0900

    feat/attitude/#218 repository 구현

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/domain/repository/WorkAttitudeApprovalRequestCommandRepository.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/command/domain/repository/WorkAttitudeApprovalRequestCommandRepository.java
@@ -1,4 +1,15 @@
 package com.touchdown.perflowbackend.workAttitude.command.domain.repository;
 
+import com.touchdown.perflowbackend.workAttitude.command.domain.aggregate.*;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 public interface WorkAttitudeApprovalRequestCommandRepository {
+
+    Optional<ApprovalRequest> findById(Long requestId);
+
+    ApprovalRequest save(ApprovalRequest approvalRequest);
+
+    boolean existsByEmpIdAndRequestTypeAndStatusAndDeleteStatusAndCreateDatetimeBetween(String empId, RequestType requestType, Status status, DeleteStatus deleteStatus, LocalDateTime startDate, LocalDateTime endDate);
 }
--- perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/query/dto/WorkAttitudeApprovalRequestResponseDTO.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/workAttitude/query/dto/WorkAttitudeApprovalRequestResponseDTO.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 
 @Data
 @Builder
-public class WorkAttitudeApprovalResponseDTO {
+public class WorkAttitudeApprovalRequestResponseDTO {
 
     private Long requestId;               // 요청 ID
 
