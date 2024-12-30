d99b82e0a7a6c8ad588a6003f17c6263b5dbbdba
JungUiJin <yealkki38@gmail.com>
2024-12-30 17:00:31 +0900

    deploy: kubernetes의 생명 확인 엔드포인트 허용

--- perflow-backend/src/main/java/com/touchdown/perflowbackend/security/config/SecurityConfig.java
+++ perflow-backend/src/main/java/com/touchdown/perflowbackend/security/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
 
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize ->
-                        authorize.requestMatchers("/swagger-ui/**", "/v3/api-docs/**","/api/v1/employee/pwd","/api/v1/login", "/api/v1/logout").permitAll()
+                        authorize.requestMatchers("/health", "/actuator/health","/swagger-ui/**", "/v3/api-docs/**","/api/v1/employee/pwd","/api/v1/login", "/api/v1/logout").permitAll()
                                 .requestMatchers("/api/v1/emp/**").hasAnyRole("EMPLOYEE","MASTER")
                                 .requestMatchers("/api/v1/hr/**").hasAnyRole("HR","MASTER")
                                 .requestMatchers("/api/v1/leader/**").hasAnyRole("LEADER","MASTER")
